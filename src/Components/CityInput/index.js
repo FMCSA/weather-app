f736a64ce3a1d6cd684ce450e933bda7032bf72c
Filipe <filipecsalves@gmail.com>
2020-02-03 23:08:06 +0000

    Adding style to input

--- src/Components/CityInput/index.js
+++ src/Components/CityInput/index.js
@@ -1,12 +1,22 @@
 import React from "react";
 
+const style = {
+  display: "flex",
+  margin: "auto",
+  padding: "10px",
+  borderWidth: "1px",
+  borderStyle: "solid",
+  borderColor: "lightgray"
+};
+
 const CityInput = props => {
-
-
-
-  return <input type="text" value={props.city} onChange={(e) => {
-    props.setCity(e.target.value);
-  }}/>;
+  return <input type="text"
+    style={style}
+    placeholder="Type to Search"
+    value={props.city}
+    onChange={(e) => {
+      props.setCity(e.target.value);
+    }} />;
 };
 
 export default CityInput;
